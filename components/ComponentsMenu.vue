cfdac4e2722ec1072a5c6eebd7dbc68f4a9cdbbf
Mira Fedas <myrosvas@gmail.com>
2020-03-05 14:36:23 +0200

    added menu and cards

--- components/ComponentsMenu.vue
+++ components/ComponentsMenu.vue
@@ -1,21 +1,60 @@
 <template>
-  <v-menu class="components-menu__wrapper">
+  <v-menu
+    class="components-menu__wrapper"
+    :close-on-click="closeOnClick"
+    :close-on-content-click="closeOnContentClick"
+    max-width="40%"
+    min-width="30%"
+    left
+  >
     <template v-slot:activator="{ on }">
-      <v-btn color="primary" dark v-on="on">
-        Open menu
+      <v-btn color="primary" v-on="on">
+        Choose components
       </v-btn>
     </template>
-    <v-list>
-      <v-list-item
-        v-for="category in categories"
-        :key="category[0]"
-        @click="selectType"
+    <v-card>
+      <v-tabs
+        v-model="tab"
+        background-color="green darken-4"
+        color="light-green"
+        dark
+        vertical
+        class="tabs-wrapper"
       >
-        <v-list-item-title>
+        <v-tab
+          v-for="category in categories"
+          :key="category"
+          @click="selectType(category)"
+        >
           {{ category }}
-        </v-list-item-title>
-      </v-list-item>
-    </v-list>
+        </v-tab>
+        <v-tabs-items v-model="tab" vertical class="tab-items-wrapper">
+          <v-tab-item v-for="item in selectedCategoryItems" :key="item">
+            <v-card flat class="components-menu__heading">
+              <v-card-text class="title" size="18px">
+                Add {{ selectedCategory }}
+              </v-card-text>
+              <v-icon @click="$emit('closeMenu')">mdi-close-circle</v-icon>
+            </v-card>
+            <v-divider></v-divider>
+            <v-card flat>
+              <v-card-actions class="components-menu__btn-container">
+                <v-btn
+                  v-for="type in selectedCategoryItems"
+                  :key="type"
+                  height="120px"
+                  width="120px"
+                  :color="componentTypes[selectedCategory][type].color"
+                  @click="selectComponent(type)"
+                >
+                  {{ type }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-tabs>
+    </v-card>
   </v-menu>
 </template>
 
@@ -25,20 +64,33 @@ import { componentTypes } from '~/config/componentTypes.config'
 export default {
   data: () => {
     return {
-      selectedType: ''
+      tab: null,
+      selectedCategoryItems: [],
+      selectedType: '',
+      selectedCategory: '',
+      selectedComponent: '',
+      closeOnClick: false,
+      closeOnContentClick: false,
+      componentTypes
     }
   },
   computed: {
     categories() {
       return Object.keys(componentTypes)
-    }
-  },
-  methods: {
-    selectType() {
-      console.log(this.selectedType)
     },
     getComponents(category) {
       return Object.keys(componentTypes[category])
+    }
+  },
+  methods: {
+    selectType(category) {
+      this.selectedCategoryItems = Object.keys(componentTypes[category])
+      this.selectedCategory = category
+      console.log(this.selectedCategoryItems)
+    },
+    selectComponent(type) {
+      this.selectedComponent = type
+      console.log(this.selectedComponent)
     }
   }
 }
@@ -48,5 +100,10 @@ export default {
 .components-menu__wrapper {
   display: flex;
   max-width: 300px;
+}
+
+.components-menu__heading {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
