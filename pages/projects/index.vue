85e894ea58c8c6c5493fda30fd4cfaaf3907e304
Mira Fedas <myrosvas@gmail.com>
2020-03-06 12:48:24 +0200

    initial commit

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,23 +1,148 @@
 <template>
-  <div>
-    <ProjectCard v-for="item in projects" :key="item.id" :item="item" />
-  </div>
+  <v-data-table
+    :headers="headers"
+    :items="projects"
+    sort-by="name"
+    class="elevation-1"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>All projects</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn
+              color="primary"
+              dark
+              class="mb-2"
+              v-on="on"
+              @click="convertDate('2015-04-23T18:25:43.511Z')"
+            >
+              New Project
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.name"
+                      label="Project name"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.created"
+                      label="Created"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.modified"
+                      label="Modified"
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      v-model="editedItem.actions"
+                      label="Actions"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.action="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">
+        mdi-pencil
+      </v-icon>
+      <v-icon small @click="deleteItem(item)">
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary">Reset</v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 import { createNamespacedHelpers } from 'vuex'
-import ProjectCard from '~/components/ProjectCard.vue'
 
 const { mapGetters, mapActions } = createNamespacedHelpers('projects')
 
 export default {
-  components: {
-    ProjectCard
+  data: () => {
+    return {
+      dialog: false,
+      headers: [
+        {
+          text: 'Name',
+          align: 'start',
+          sortable: true,
+          value: 'name'
+        },
+        {
+          text: 'Created',
+          align: 'start',
+          sortable: true,
+          value: 'created'
+        },
+        {
+          text: 'Modified',
+          align: 'start',
+          sortable: true,
+          value: 'modified'
+        },
+        {
+          text: 'Actions',
+          align: 'start',
+          sortable: false,
+          value: 'actions'
+        }
+      ],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+        created: 0,
+        modified: 0,
+        actions: null
+      },
+      defaultItem: {
+        name: '',
+        created: 0,
+        modified: 0,
+        actions: null
+      }
+    }
   },
   computed: {
     ...mapGetters({
       projects: 'filtered'
-    })
+    }),
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    }
+  },
+  watch: {
+    dialog(val) {
+      val || this.close()
+    }
   },
   mounted() {
     this.fetch()
@@ -25,7 +150,40 @@ export default {
   methods: {
     ...mapActions({
       fetch: 'get'
-    })
+    }),
+    convertDate(date) {
+      if (date) {
+        return date.slice(0, 19).replace(/T/g, ' ')
+      }
+    },
+    editItem(item) {
+      // redirection!!!
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      const index = this.projects.indexOf(item)
+      confirm('Are you sure you want to delete this item?') &&
+        this.projects.splice(index, 1)
+    },
+
+    close() {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      }, 300)
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
+      }
+      this.close()
+    }
   }
 }
 </script>
