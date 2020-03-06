f15f5f7a265cd0f2e51c6b1c28fa9ae3ace868d9
Andrii Veremchuk <andveremchuk@gmail.com>
2020-03-06 16:41:50 +0200

    attach preset

---
+++ config/preset.config.js
@@ -0,0 +1,554 @@
+export const presets = [
+  {
+    columns: 1,
+    rows: 1,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 2,
+    rows: 1,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 2,
+            y: 3
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 3,
+    rows: 1,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 2,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 3
+          },
+          end: {
+            x: 2,
+            y: 4
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 4,
+    rows: 1,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 2,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 3
+          },
+          end: {
+            x: 2,
+            y: 4
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 4
+          },
+          end: {
+            x: 2,
+            y: 5
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 3,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 3,
+            y: 4
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 3,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 3
+          },
+          end: {
+            x: 2,
+            y: 4
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 3
+          },
+          end: {
+            x: 3,
+            y: 4
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 4,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 2,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 2
+          },
+          end: {
+            x: 3,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 3
+          },
+          end: {
+            x: 3,
+            y: 5
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 4,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 3
+          },
+          end: {
+            x: 2,
+            y: 4
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 4
+          },
+          end: {
+            x: 2,
+            y: 5
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 3
+          },
+          end: {
+            x: 3,
+            y: 4
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 4
+          },
+          end: {
+            x: 3,
+            y: 5
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 2,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 2
+          },
+          end: {
+            x: 3,
+            y: 3
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 2,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 2,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 2
+          },
+          end: {
+            x: 3,
+            y: 3
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 3,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 3,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 3
+          },
+          end: {
+            x: 3,
+            y: 4
+          }
+        }
+      }
+    ]
+  },
+  {
+    columns: 4,
+    rows: 2,
+    zones: [
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 1
+          },
+          end: {
+            x: 2,
+            y: 2
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 2
+          },
+          end: {
+            x: 2,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 2,
+            y: 1
+          },
+          end: {
+            x: 3,
+            y: 3
+          }
+        }
+      },
+      {
+        coords: {
+          start: {
+            x: 1,
+            y: 3
+          },
+          end: {
+            x: 3,
+            y: 5
+          }
+        }
+      }
+    ]
+  }
+]
