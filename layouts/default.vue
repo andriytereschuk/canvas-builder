ac80617ee13ab315acfa32656f9ed0db6e6a0237
Andrii Veremchuk <andveremchuk@gmail.com>
2020-03-19 19:50:15 +0200

    project navbar implemented

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,15 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <!-- <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn> -->
-      <!-- <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn> -->
-      <v-breadcrumbs :items="breadcrumbs">
-        <v-breadcrumbs-item
-          slot="item"
-          slot-scope="{ item }"
-          :href="getLink(item)"
-        >
-          <input
-            v-if="edit && item.text === 'projectID'"
-            v-focus=""
-            type="text"
-            :value="name"
-            @blur="
-              updateProjectName($event.target.value)
-              edit = false
-            "
-            @keyup.enter="
-              updateProjectName($event.target.value)
-              edit = false
-            "
-          />
-          <p
-            v-else=""
-            class="breadcrumbs-item"
-            data-hint="rename"
-            :class="item.text === 'projectID' ? 'tooltip tooltip--top' : false"
-            @click="name ? (edit = true) : false"
-          >
-            {{ item.text === 'projectID' ? name : item.text }}
-          </p>
-        </v-breadcrumbs-item>
-      </v-breadcrumbs>
-      <v-spacer />
-      <v-btn text style="width: 100px; margin-right: 0;" @click="save">
-        <v-icon>mdi-save</v-icon>
-        save
-      </v-btn>
-      <v-btn text style="width: 100px; margin-right: 0;">
-        <v-icon>mdi-play</v-icon>
-        preview
-      </v-btn>
-
-      <!-- <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn> -->
+      <nuxt-link to="/">
+        <img class="logo" src="../assets/logo.png" alt="site-logo" />
+      </nuxt-link>
     </v-app-bar>
-
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -120,22 +18,12 @@
 
 <script>
 import { createNamespacedHelpers } from 'vuex'
-const { mapGetters, mapMutations, mapActions } = createNamespacedHelpers(
-  'project'
-)
+const { mapMutations, mapActions } = createNamespacedHelpers('project')
 
 export default {
-  directives: {
-    focus: {
-      inserted(el) {
-        el.focus()
-      }
-    }
-  },
   middleware: 'redirect',
   data() {
     return {
-      edit: false,
       clipped: false,
       drawer: false,
       fixed: false,
@@ -148,30 +36,25 @@ export default {
       ],
       miniVariant: false,
       right: true,
-      rightDrawer: false,
-      breadcrumbs: [
-        {
-          text: 'Canvas Builder',
-          disabled: false,
-          href: '/'
-        },
-        {
-          text: 'Projects',
-          disabled: false,
-          href: '/projects'
-        },
-        {
-          text: 'projectID',
-          disabled: true,
-          href: ''
-        }
-      ]
+      rightDrawer: false
+      // breadcrumbs: [
+      //   {
+      //     text: 'Canvas Builder',
+      //     disabled: false,
+      //     href: '/'
+      //   },
+      //   {
+      //     text: 'Projects',
+      //     disabled: false,
+      //     href: '/projects'
+      //   },
+      //   {
+      //     text: 'projectID',
+      //     disabled: true,
+      //     href: ''
+      //   }
+      // ]
     }
-  },
-  computed: {
-    ...mapGetters({
-      name: 'getProjectName'
-    })
   },
   methods: {
     ...mapActions(['save']),
@@ -186,59 +69,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.breadcrumbs-item {
-  margin-bottom: 0;
-}
-.tooltip {
-  position: relative;
-  display: inline-block;
-}
-.tooltip:before,
-.tooltip:after {
-  position: absolute;
-  opacity: 0;
-  pointer-events: none;
-}
-.tooltip:hover:before,
-.tooltip:hover:after {
-  opacity: 1;
-}
-.tooltip:before {
-  content: '';
-  position: absolute;
-  background: #bdbdbd;
-  width: 6px;
-  height: 6px;
-  transform: translateX(-50%) translateY(-45%) rotate(225deg);
-  z-index: 2;
-}
-.tooltip:after {
-  content: attr(data-hint);
-  z-index: 1;
-  padding: 5px 6px;
-  background: #bdbdbd;
-  color: black;
-  text-transform: capitalize;
-  font-size: 12px;
-  white-space: nowrap;
-  transform: translateX(-50%);
-  border-radius: 5px;
-}
-.tooltip--top:before,
-.tooltip--top:after {
-  top: 130%;
-  left: 50%;
-}
-.tooltip--top:hover:before {
-  width: 12px;
-  height: 12px;
-  border: 1px solid #f0f0f0;
-  border-width: 0 1px 1px 0;
-  border-top-left-radius: 100%;
-}
-.tooltip--top:hover:after {
-  border-width: 5px;
-  border-top-color: rgb(29, 104, 189);
-  border: 1px solid #f0f0f0;
+.logo {
+  display: block;
+  width: 50px;
+  height: 44px;
 }
 </style>
--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    <Stepper>
+    <template>
+      <v-card color="grey lighten-4">
+        <v-toolbar dense>
+          <input
+            v-if="edit"
+            v-focus=""
+            type="text"
+            :value="name"
+            class="project-name"
+            @blur="
+              setProjectName($event.target.value)
+              edit = false
+            "
+            @keyup.enter="
+              setProjectName($event.target.value)
+              edit = false
+            "
+          />
+          <v-tooltip v-else top>
+            <template v-slot:activator="{ on }">
+              <v-toolbar-title @click="edit = true" v-on="on">
+                {{ name }}
+              </v-toolbar-title>
+            </template>
+            <span>Rename</span>
+          </v-tooltip>
+
+          <v-spacer></v-spacer>
+
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-btn icon @click="save" v-on="on">
+                <v-icon>fas fa-save</v-icon>
+              </v-btn>
+            </template>
+            <span>Save</span>
+          </v-tooltip>
+
+          <v-tooltip top>
+            <template v-slot:activator="{ on }">
+              <v-btn icon v-on="on">
+                <v-icon>far fa-eye</v-icon>
+              </v-btn>
+            </template>
+            <span>Preview</span>
+          </v-tooltip>
+        </v-toolbar>
+      </v-card>
+    </template>
+    <Stepper style="marginBottom: 20 + 'px'">
       <ComponentsMenu ref="componentsMenu" />
       <Workspace @addPreset="addPreset" />
       <PresetList ref="presets" @openCustomPreset="openCustomPreset" />
@@ -10,7 +59,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
 import CustomPreset from '~/components/CustomPreset'
 import Stepper from '~/components/Stepper'
 import ComponentsMenu from '~/components/ComponentsMenu'
@@ -25,6 +74,23 @@ export default {
     PresetList,
     CustomPreset
   },
+  directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      }
+    }
+  },
+  data() {
+    return {
+      edit: false
+    }
+  },
+  computed: {
+    ...mapGetters('project', {
+      name: 'getProjectName'
+    })
+  },
   created() {
     this.fetchProject(+this.$route.params.id)
   },
@@ -34,7 +100,8 @@ export default {
     }
   },
   methods: {
-    ...mapActions('project', ['fetchProject']),
+    ...mapActions('project', ['fetchProject', 'save']),
+    ...mapMutations('project', ['updateProjectName']),
     addPreset() {
       return this.$refs.presets.open()
     },
@@ -44,9 +111,12 @@ export default {
     },
     closeCustomPreset() {
       this.$refs.customPreset.close()
+    },
+    setProjectName(name) {
+      if (name) {
+        this.updateProjectName(name)
+      }
     }
   }
 }
 </script>
-
-<style></style>
