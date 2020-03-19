22fbc60550272ff08cafd2df3bbde8cfa6315f17
Iryna <imukhina@griddynamics.com>
2020-03-19 19:45:52 +0200

    correct default product img url

--- config/component.config.js
+++ config/component.config.js
@@ -145,7 +145,7 @@ export const componentConfig = {
         title: '',
         subtitle: '',
         description: '',
-        image: '',
+        image: ``,
         price: '',
         rating: true,
         actionIcons: {
